fb48b12f95eb2c6b54c639e717be5880ca652679
Zee <76219804+Mo-Xiyad@users.noreply.github.com>
2021-11-16 17:29:42 +0100

    users validator

--- src/services/users/validator.js
+++ src/services/users/validator.js
@@ -1,7 +1,8 @@
-import { checkSchema, body, validationResult } from "express-validator";
+import { body } from "express-validator";
 
 export const userValidator = [
   body("name").exists().withMessage("Name is required!"),
+  body("name").isLength({ min: 2 }).withMessage("must more than 2 characters!"),
   body("surname").exists().withMessage("Surname is required!"),
   body("email").isEmail().withMessage("Not a Valid Email!"),
 ];
