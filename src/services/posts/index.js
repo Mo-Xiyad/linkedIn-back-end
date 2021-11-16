1e1f0f5f7f77507799314698b88811771cd7015b
Zee <76219804+Mo-Xiyad@users.noreply.github.com>
2021-11-16 01:25:23 +0100

    likes

--- src/services/posts/index.js
+++ src/services/posts/index.js
@@ -3,6 +3,9 @@ import handler from "./handlers.js";
 const router = express.Router();
 router.route(`/`).get(handler.getPosts).post(handler.createPosts);
 
+// LIKES
+router.route("/:postId/likes").put(handler.likePost);
+
 router
   .route(`/:postId`)
   .get(handler.getPostsById)
