d7f18695f415f0d6ecac235c97c339d5543e1ff2
Zee <76219804+Mo-Xiyad@users.noreply.github.com>
2022-02-05 20:27:41 +0100

    text limit on post changes to 2 chrs

--- src/services/posts/validator.js
+++ src/services/posts/validator.js
@@ -2,7 +2,7 @@ import { body } from "express-validator";
 
 export const postCheck = [
   body("text").exists().withMessage("text is a mandatory field!"),
-  body("text").isLength({ min: 20 }).withMessage("Post is too sort "),
+  body("text").isLength({ min: 2 }).withMessage("Post is too sort "),
   body("user").exists().withMessage("User Id is required "),
 ];
 
